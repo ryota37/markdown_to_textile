d0e8640c0de11931ab5ec40e71091c4d445c7dfc
Ryota.Nakahama <ryota.nakahama@access-company.com>
2024-08-28 16:33:10 +0900

    Insert line break after code block

--- markdown_to_textile_codeblock.py
+++ markdown_to_textile_codeblock.py
@@ -1,15 +1,17 @@
 import panflute as pf
 
+
 def action(elem, doc):
     if isinstance(elem, pf.CodeBlock):
         # 言語指定がある場合は取得し、classとして追加
         language = elem.classes[0] if elem.classes else ''
         # Textile形式の<pre><code>ブロックに変換
-        return pf.RawBlock(f'<pre><code class="{language}">\n{elem.text}\n</code></pre>', format='textile')
+        return pf.RawBlock(f'<pre><code class="{language}">\n{elem.text}\n</code></pre>\n', format='textile')
+
 
 def main(doc=None):
     return pf.run_filter(action, doc=doc)
 
+
 if __name__ == "__main__":
     main()
-
